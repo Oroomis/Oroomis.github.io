1dc57a7b0acf1be0d5c368ce5fa6970fa371cb59
Oroomis <rohere@op.pl>
2023-05-03 15:05:13 +0200

    modified python code, prepared for mpl_interactions

--- home/infographics/main.py
+++ home/infographics/main.py
@@ -1,20 +1,18 @@
 import mpl_interactions.ipyplot as iplt
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+x = np.linspace(0, np.pi, 100)
+l = 3
+
+def f1(x, l):
+    return x*l
+
 fig, ax = plt.subplots()
 
-year_1 = [2016, 2017, 2018, 2019, 2020, 2021]
-population_1 = [42, 43, 45, 47, 48, 50]
+controls = iplt.plot(x, f1, l=l, label="f1")
+iplt.plot(x, f1, controls=controls, label="f1")
+_ = plt.legend()
+plt.show()
 
-year_2 = [2016, 2017, 2018, 2019, 2020, 2021]
-population_2 = [43, 43, 44, 44, 45, 45]
-
-plt.plot(year_1, population_1, marker='o', linestyle='--', color='g', label='Country 1')
-plt.plot(year_2, population_2,  marker='d', linestyle='-', color='r', label='Country 2')
-
-plt.xlabel('Year')
-plt.ylabel('Population (M)')
-plt.title('Year vs Population')
-plt.legend(loc='lower right')
-
-display(fig, target="mpl")
